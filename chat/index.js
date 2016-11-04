eb9c571557960fc75f609883c1afd4cdc07ab920
dotask <macspirit@MacBook-Air-MacSpirit.local>
2016-11-03 23:27:32 -0700

    7
    change port

--- chat/index.js
+++ chat/index.js
@@ -5,6 +5,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     //res.sendfile('index.html');
@@ -17,4 +18,4 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000);
+http.listen(port);
